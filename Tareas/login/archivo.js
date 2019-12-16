63fbb5515447d67a1212c8ea42050da78a19cfb4
Johan Herrera <johanhg1024@gmail.com>
2019-12-15 21:26:53 -0600

    login

---
+++ Tareas/login/archivo.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    alert("Se ha registrado con éxito!");
+    window.location("indice.html");
+  }
+
+  function myFunction2() {
+    alert("Se ha cambiado la contraseña con éxito!");
+    window.location("indice.html");
+  }  
